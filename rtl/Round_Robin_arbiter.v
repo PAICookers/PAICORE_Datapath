4f8d07a0ba5d58e138876452e2f81b4a24d0f5f9
anne <1005593730@qq.com>
2023-12-06 17:06:12 +0800

    add Round_Robin_arbiter_base

--- rtl/Round_Robin_arbiter.v
+++ rtl/Round_Robin_arbiter.v
@@ -1,33 +1,33 @@
-// module Round_Robin_arbiter#(
-//     parameter NUM_REQ = 4
-// )(
-//     input                   clk,
-//     input                   rst_n,
-//     input                   arb_enable,
-//     input  [NUM_REQ-1:0]    request,
-//     output [NUM_REQ-1:0]    grant
-// );
+module Round_Robin_arbiter_base#(
+    parameter NUM_REQ = 4
+)(
+    input                   clk,
+    input                   rst_n,
+    input                   arb_enable,
+    input  [NUM_REQ-1:0]    request,
+    output [NUM_REQ-1:0]    grant
+);
 
-//     reg [NUM_REQ-1:0]   hist_req;
+    reg [NUM_REQ-1:0]   hist_req;
 
-//     always @(posedge clk) begin
-//         if(!rst_n) begin
-//             hist_req <= {{NUM_REQ-1{1'b0}}, 1'b1};
-//         end else if(|request) begin
-//             hist_req <= {grant[NUM_REQ-2:0], grant[NUM_REQ-1]};
-//         end
-//     end
+    always @(posedge clk) begin
+        if(!rst_n) begin
+            hist_req <= {{NUM_REQ-1{1'b0}}, 1'b1};
+        end else if(|request) begin
+            hist_req <= {grant[NUM_REQ-2:0], grant[NUM_REQ-1]};
+        end
+    end
 
-//     Fixed_arbiter_base #(
-//         .NUM_REQ    (NUM_REQ    )
-//     ) u_Fixed_arbiter_base(
-//         .arb_enable (arb_enable ),
-//         .base       (hist_req     ),
-//         .request    (request    ),
-//         .grant      (grant      )
-//     );
+    Fixed_arbiter_base #(
+        .NUM_REQ    (NUM_REQ    )
+    ) u_Fixed_arbiter_base(
+        .arb_enable (arb_enable ),
+        .base       (hist_req     ),
+        .request    (request    ),
+        .grant      (grant      )
+    );
 
-// endmodule
+endmodule
 
 
 module Round_Robin_arbiter#(
